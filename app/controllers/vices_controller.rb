ab65b9fddf8904745737a7c7e32eedbda2fc2de3
quentin de malherbe <quentdemalherbe@gmail.com>
2020-02-19 23:18:28 +0100

    filtre

--- app/controllers/vices_controller.rb
+++ app/controllers/vices_controller.rb
@@ -2,8 +2,28 @@ class VicesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @vices = Vice.geocoded #returns flats with coordinates
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR user ILIKE :query OR address ILIKE :query"
+      @vices = Vice.geocoded.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @vices = Vice.geocoded
+    end
 
+
+    if params[:query].present?
+      sql_query = " \
+        vices.name @@ :query \
+        OR vices.category @@ :query \
+        OR vices.address @@ :query \
+        OR users.first_name @@ :query \
+        OR users.last_name @@ :query \
+      "
+      @vices = Vice.geocoded.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @vices = Vice.geocoded
+    end
+
+    # @vices = Vice.geocoded #returns flats with coordinates
     @markers = @vices.map do |vice|
       {
         lat: vice.latitude,
