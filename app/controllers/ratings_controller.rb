de18f1d4a9e354b03b737d1ad7048e2cd79ae515
Marceau Tassin <marceau.tassin@gmail.com>
2020-02-21 12:51:58 +0100

    début de l'ajax

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,20 @@
 class RatingsController < ApplicationController
 
   def create
-    @rating = Rating.create(rating: params[:rating], user_id: current_user.id ,vice_id: params[:vice_id])
-    redirect_to user_path(current_user)
+    @rating = Rating.new(rating: params[:rating], user_id: current_user.id ,vice_id: params[:vice_id])
+    @user = current_user
+    if @rating.save
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render '/users/show' }
+        format.js  # <-- idem
+      end
+    end
+    # redirect_to user_path(current_user)
   end
 
 
