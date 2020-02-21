af132e5a1fc13719c83c6dfd20c39e8715cb9a5e
Marceau Tassin <marceau.tassin@gmail.com>
2020-02-21 01:05:38 +0100

    tentatives de JS

--- app/javascript/plugins/init_rating_hoover.js
+++ app/javascript/plugins/init_rating_hoover.js
@@ -1,17 +1,26 @@
 //constant definition
-const rating_1 = document.getElementById('rating_1')
-const rating_2 = document.getElementById('rating_2')
-const rating_3 = document.getElementById('rating_3')
-const rating_4 = document.getElementById('rating_4')
-const rating_5 = document.getElementById('rating_5')
+const rate_1 = document.getElementById('rate_1')
+const rate_2 = document.getElementById('rate_2')
+const rate_3 = document.getElementById('rate_3')
+const rate_4 = document.getElementById('rate_4')
+const rate_5 = document.getElementById('rate_5')
 
 // function definition
 const hooverRatings = () => {
-  const boobs = `<%= image_tag('boobSelect.png') %>`;
-  rating_1.innerHTML = boobs
+  const boobs = `<img src="/assets/boobsSelect-6dd2522ce41418acc9e53aa976a2f5e9fd5d458b6fe55354a1ce98f5e8244085.png">`;
+  rate_1.innerHTML.replace = boobs
 };
 
 //listener
-rating_1.addEventListener("click", (event) => {
+rate_1.addEventListener("click", (event) => {
+  event.preventDefault();
   hooverRatings()
 });
+
+const changeimg(boobs) => {
+    boobs.src = (boobs.src=="/assets/boobsUnselect-84abdc325e54acd52e8670687761db4a19576b953a6da3eb5092df88d740d89d.png" ) ? "/assets/boobsUnselect-84abdc325e54acd52e8670687761db4a19576b953a6da3eb5092df88d740d89d.png" : "/assets/boobsSelect-6dd2522ce41418acc9e53aa976a2f5e9fd5d458b6fe55354a1ce98f5e8244085.png";
+} ;
+
+const changeimg = (boobs) => {
+    boobs.src = (boobs.src=="/assets/boobsUnselect-84abdc325e54acd52e8670687761db4a19576b953a6da3eb5092df88d740d89d.png" ) ? "/assets/boobsUnselect-84abdc325e54acd52e8670687761db4a19576b953a6da3eb5092df88d740d89d.png" : "/assets/boobsSelect-6dd2522ce41418acc9e53aa976a2f5e9fd5d458b6fe55354a1ce98f5e8244085.png";
+} ;
